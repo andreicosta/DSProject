062a4990f7686cf17a36b22ce913fdda481bb663
Felipe <felipe@felipe.(none)>
2013-07-10 12:40:47 -0300

    modificado o bem vindo

--- DSProject/src/Interface/WelcomePanel.java
+++ DSProject/src/Interface/WelcomePanel.java
@@ -1,5 +1,17 @@
 package Interface;
 
+import java.awt.CardLayout;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRootPane;
+import javax.swing.text.DefaultFormatterFactory;
+import javax.swing.text.MaskFormatter;
+
 public class WelcomePanel extends javax.swing.JPanel {
 
     public WelcomePanel() {
@@ -11,18 +23,31 @@ public class WelcomePanel extends javax.swing.JPanel {
     private void initComponents() {
 
         welcomeMessage = new javax.swing.JLabel();
+        try{
+            BufferedImage pic = ImageIO.read(new File("images/roda_grande.gif"));
+            jLabel4 = new javax.swing.JLabel(new ImageIcon(pic));
 
-        setMinimumSize(new java.awt.Dimension(833, 515));
-        setPreferredSize(new java.awt.Dimension(833, 515));
-        setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+            setMinimumSize(new java.awt.Dimension(833, 515));
+            setPreferredSize(new java.awt.Dimension(833, 515));
+            setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        welcomeMessage.setFont(new java.awt.Font("Ubuntu", 0, 18)); // NOI18N
-        welcomeMessage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        welcomeMessage.setText("Bem Vindo ao PRODOWN");
-        welcomeMessage.setPreferredSize(new java.awt.Dimension(833, 21));
-        add(welcomeMessage, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 20, -1, -1));
+            welcomeMessage.setFont(new java.awt.Font("Ubuntu", 1, 18)); // NOI18N
+            welcomeMessage.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+            welcomeMessage.setText("Bem Vindo ao PRODOWN");
+            welcomeMessage.setPreferredSize(new java.awt.Dimension(833, 21));
+            add(welcomeMessage, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 440, -1, -1));
+
+            /*
+            jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+            jLabel4.setText(null);
+            */
+        }catch(Exception e){
+            jLabel4.setText("");
+        }
+        add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(-80, 70, 995, 330));
     }// </editor-fold>//GEN-END:initComponents
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel welcomeMessage;
     // End of variables declaration//GEN-END:variables
 
